b646d6983c43761ee18ad135dbea7014c87b7c41
Ko-Kaung <111555341+Ko-Kaung@users.noreply.github.com>
2022-08-20 12:23:18 +0630

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+// let quotesDataa=[];
+// async function quoteGenerator(){
+//     const apiUrl = "https://gist.githubusercontent.com/nasrulhazim/54b659e43b1035215cd0ba1d4577ee80/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json";
+//     try{
+//         const resopnse = await fetch(apiUrl);
+//          quotesDataa =  await resopnse.json();
+//          console.log(quotesDataa[1]);
+//     }
+//     catch(error){
+//         console.log(error)
+//     }
+// }
+let quoteData = [] ;
+
+async function quoteGenerator(){
+    const apiUrl = "https://quoite.api/netify.com";
+    try{
+        const response = await fetch(apiUrl);
+        quoteData = await response.json();
+        alert(quoteData[Math.floor(Math.random() * quoteData.length)]);
+    }catch(error){
+        console.log(error);
+    }
+}
+
+ 
